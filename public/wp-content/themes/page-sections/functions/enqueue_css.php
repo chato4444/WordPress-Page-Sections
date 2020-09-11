0bc253de39acc8191fd56fe81bafd81dc28b513d
Chato Ramirez <chato@swingu.com>
2020-09-11 16:55:03 -0400

    ST-5 Add icon grid section module

--- public/wp-content/themes/page-sections/functions/enqueue_css.php
+++ public/wp-content/themes/page-sections/functions/enqueue_css.php
@@ -7,22 +7,12 @@ add_action('wp_enqueue_scripts', function() {
 	wp_dequeue_style( 'wp-block-library-theme' );
 	wp_dequeue_style( 'wc-block-style' );
 
+    // https://linearicons.com/free
+    wp_register_style( 'linear-icons', 'https://cdn.linearicons.com/free/1.0.0/icon-font.min.css', '', '1.0', 'all' );
+    wp_enqueue_style( 'linear-icons' );
+
     wp_register_style( 'main', Asset_Version::getVersionParam( '/css/main.css' ) );
     wp_enqueue_style( 'main' );
-
-	// EXTERNAL EXAMPLES
-	/*
-	wp_register_style( 'ico-moon', 'https://s3.amazonaws.com/icomoon.io/123456/EXAMPLE/style.css?331cn9', '', '1.0', 'all' );
-	wp_enqueue_style( 'ico-moon' );
-	*/
-
-	/*
-	wp_register_style( 'swiper-js', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.6/css/swiper.min.css', '', '1.0', 'all' );
-	wp_enqueue_style( 'swiper-js' );
-	*/
-
-
-
 }, 101);
 
 /**
--- public/wp-content/themes/page-sections/functions/enqueue_fonts.php
+++ public/wp-content/themes/page-sections/functions/enqueue_fonts.php
@@ -1,6 +1,6 @@
 <?php
 add_action('wp_enqueue_scripts', 'sxs_enqueue_fonts');
 function sxs_enqueue_fonts() {
-	wp_register_style( 'sxs-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i&display=swap', false );
+	wp_register_style( 'sxs-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i,900&display=swap', false );
 	wp_enqueue_style( 'sxs-google-fonts');
 }
